3a9046fe0522f99f6949116db3e8860bb87c614b
Chaitu269 <122653347+Chaitu269@users.noreply.github.com>
2023-04-25 00:45:49 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = '7956606'
+API_HASH = 'f74d79be8f8d2c375afb1816faf36449'
+BOT_TOKEN = '6199534884:AAHXVZdWbzw-RZWuyrU4fAM0nwIlPstFkjQ'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,13 +11,13 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1422130189]
+CHANNELS = [-1001820336218]
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb://Chaitu24:Chaitu24@ac-foedfhg-shard-00-00.2jcanuf.mongodb.net:27017,ac-foedfhg-shard-00-01.2jcanuf.mongodb.net:27017,ac-foedfhg-shard-00-02.2jcanuf.mongodb.net:27017/?ssl=true&replicaSet=atlas-lg6fvx-shard-0&authSource=admin&retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
