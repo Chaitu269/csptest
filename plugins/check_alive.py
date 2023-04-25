af2ed488656636237aca8648f6de0d27ee9ee556
Chaitu269 <122653347+Chaitu269@users.noreply.github.com>
2023-04-26 03:24:25 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -18,9 +18,7 @@ CMD = ["/", "." " "]
 
 @Client.on_message(filters.command("deepl1" , CMD))
 async def deepl1(_, message):
-    await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("VIP 1",callback_data = "d1"), 
-        			InlineKeyboardButton("VIP 2",callback_data = "d1") ]]))
+    await message.reply_text("Select Plan.........")
 
 
 
