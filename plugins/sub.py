189be2e9c4e871182e5105a791156b3e4a97dfdc
Chaitu269 <122653347+Chaitu269@users.noreply.github.com>
2023-04-27 01:52:40 +0530

    Update sub.py

--- plugins/sub.py
+++ plugins/sub.py
@@ -38,10 +38,3 @@ async def deepl1(_, message):
                 ]])
 
 
-@Client.on_message(filters.command("ping", CMD))
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...........")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"𝖯𝗂𝗇𝗀!\n{time_taken_s:.3f} ms")
