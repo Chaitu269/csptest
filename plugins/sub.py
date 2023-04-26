4405df1b420eae59053640d2dea3b73644f380bc
Chaitu269 <122653347+Chaitu269@users.noreply.github.com>
2023-04-27 01:46:11 +0530

    Update sub.py

--- plugins/sub.py
+++ plugins/sub.py
@@ -9,23 +9,33 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 CMD = ["/", "." , ""]
 
 
-@Client.on_message(filters.command("alive", CMD))
-async def check_alive(_, message):
-    await message.reply_text("𝖡𝗎𝖽𝖽𝗒 𝖨𝖺𝗆 𝖠𝗅𝗂𝗏𝖾 :) 𝖧𝗂𝗍 /start \n\n𝖧𝗂𝗍 /help 𝖥𝗈𝗋 𝖧𝖾𝗅𝗉 ;)\n\n\n𝖧𝗂𝗍 /ping 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖡𝗈𝗍 𝖯𝗂𝗇𝗀 😁")
+
 
 @Client.on_message(filters.command("help", CMD))
 async def help(_, message):
-    await message.reply_text("𝖧𝗂𝗍 /movie 𝖥𝗈𝗋 𝖬𝗈𝗏𝗂𝖾 𝖲𝖾𝖺𝗋𝖼𝗁 𝖱𝗎𝗅𝖾𝗌 📃\n\n𝖧𝗂𝗍 /series 𝖥𝗈𝗋 𝖲𝖾𝗋𝗂𝖾𝗌 𝖲𝖾𝖺𝗋𝖼𝗁 𝖱𝗎𝗅𝖾𝗌")
+    await message.reply_text("This Bot will provide you textbooks and previous quation papers \n\n Just enter the cource code or cource name you will get the text books and previous quation ppers of the cource \n\n  If you found any wrong information in this bot click /admin to send message to the admin   \n\n   If you want to add any new cource in this bot click \requst to send request to add cource  ")
 
+@Client.on_message(filters.command("admin", CMD))
+async def help(_, message):
+    await message.reply_text("if you found any wrong information in this bot  please message here @myagnasai we will update the information as sonn as possible")
 
+@Client.on_message(filters.command("request", CMD))
+async def help(_, message):
+    await message.reply_text("if you want to add any extra cource in this bot  please message here @Chaitu_1438 we will check cource details and add as soon as possible")
 
-@Client.on_message(filters.command("deepl1" , CMD))
+@Client.on_message(filters.command(["dl", "deeplearning"] , CMD))
 async def deepl1(_, message):
-    await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("VIP 1",callback_data = "vip1")],[ 
-        			InlineKeyboardButton("VIP 2",callback_data = "vip2") ]]))
-
-
+    await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[
+                    InlineKeyboardButton('Unit 1', callback_data="dlu1"),
+                    InlineKeyboardButton('Unit 2', callback_data="dlu1")
+                ],[
+                    InlineKeyboardButton('Unit 3', url=f"https://t.me/Testkarebot?start=ZmlsZV9CUUFEQlFBREVnb0FBaUZGYVZXN2JIcGxNeFNrSUJZRQ"),
+                    InlineKeyboardButton('Unit 4', callback_data='dlu4')
+                ],[
+                    InlineKeyboardButton('Unit 5', callback_data="dlu5"),
+                    InlineKeyboardButton('Previous Quation Papers ', callback_data="dlu1")
+               
+                ]])
 
 
 @Client.on_message(filters.command("ping", CMD))
